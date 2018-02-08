cdfea7f6af2caa20d4b915b8531d61f78ff6c755
wanghuiyong <weifeng19911024@163.com>
2018-02-08 14:39:30 +0800

    ptr

---
+++ 编程语言(1) C, C++/sources/c 和指针/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include "variable_argument_lists.h"
+
+/************************
+	主函数
+************************/
+int main (int argc, char *argv[])
+{
+    return 0;
+}
